142cbb79e347a28bb529cb67e2a8c5643cd58496
Justin Smith <67644167+j-smith3@users.noreply.github.com>
2021-02-16 19:16:53 -0600

    Update format_data.py

--- format_data.py
+++ format_data.py
@@ -20,38 +20,47 @@ from cleaning import cleanup, fix_names, combine_df, rename_passing, \
 os.getcwd()
 os.chdir(r'C:\Users\jds05\Desktop\qb_project')
 
-def reformat_data(year):    
+#this function cleans up the data and adds some calculated fields
+def reformat_data(year, df):    
     
-    #define file names
-    pass_file_name = str(year) + '_passing_stats.csv'
-    rush_file_name = str(year) + '_rushing_stats.csv'
+    #if year > 0, load in file and clean columns
+    if year > 0:
+        #define file names
+        pass_file_name = str(year) + '_passing_stats.csv'
+        rush_file_name = str(year) + '_rushing_stats.csv'
+            
+        #load in files
+        pass_df = pd.read_csv(pass_file_name, index_col = 0)
+        rush_df = pd.read_csv(rush_file_name, index_col = 0)
         
-    #load in files
-    pass_df = pd.read_csv(pass_file_name, index_col = 0)
-    rush_df = pd.read_csv(rush_file_name, index_col = 0)
+        #remove undesirable columns
+        pass_df, qb_names = get_qbs(pass_df)
+        pass_df = cleanup(pass_df, qb_names)
+        rush_df = cleanup(rush_df, qb_names)
+        
+        #cleanup Player name column
+        pass_df = fix_names(pass_df)    
+        rush_df = fix_names(rush_df)
+        
+        #Rename columns to better identify attributes (i.e. Yds vs Pass_Yds)
+        pass_df = rename_passing(pass_df)
+        rush_df = rename_rushing(rush_df)
+        
+        #concatenate passing and rushing dataframes
+        #skip first 3 of rushing df (Pos, G, GS)
+        pass_stats = pd.concat([pass_df, rush_df.iloc[:,3:]], axis = 1)
     
-    #remove undesirable columns
-    pass_df, qb_names = get_qbs(pass_df)
-    pass_df = cleanup(pass_df, qb_names)
-    rush_df = cleanup(rush_df, qb_names)
-    
-    #cleanup Player name column
-    pass_df = fix_names(pass_df)    
-    rush_df = fix_names(rush_df)
-    
-    #Rename columns to better identify attributes (i.e. Yds vs Pass_Yds)
-    pass_df = rename_passing(pass_df)
-    rush_df = rename_rushing(rush_df)
-    
-    #concatenate passing and rushing dataframes
-    #skip first 3 of rushing df (Pos, G, GS)
-    pass_stats = pd.concat([pass_df, rush_df.iloc[:,3:]], axis = 1)
+    #if year == -1, load in passed in dataframe
+    if year == -1:
+        pass_stats = df
     
     #add some calculated columns
-    pass_stats['Yds_Att'] = round((pass_stats['Pass_Yds'] \
-                               / pass_stats['Pass_Att']), 2)
-    pass_stats['Yds_Cmp'] = round((pass_stats['Pass_Yds'] \
-                                / pass_stats['Cmp']), 2 )
+    pass_stats['AY/A'] = round(((pass_stats['Pass_Yds'] + (20 * \
+                                 pass_stats['Pass_TD']) - (45 * \
+                                 pass_stats['Int'])) / \
+                                 (pass_stats['Pass_Att'])), 2 )   
+        
+        
     pass_stats['Total_TD'] = (pass_stats['Pass_TD'] + pass_stats['Rush_TD'])
     pass_stats['Total_Yds'] = (pass_stats['Pass_Yds'] + \
                                pass_stats['Rush_Yds'])
@@ -72,17 +81,20 @@ def reformat_data(year):
     pass_stats['Tot_Yds_Gm'] = round(((pass_stats['Pass_Yds'] + \
                                        pass_stats['Rush_Yds']) / \
                                       pass_stats['G']), 4) 
+    pass_stats['1D/G'] = round((pass_stats['Total_1D'] / pass_stats['G']), 2)    
+    pass_stats['TD/G'] = round(((pass_stats['Pass_TD'] + \
+                                 pass_stats['Rush_TD']) / pass_stats['G']), 2)
     
     pass_stats = pass_stats.fillna(value = 0)
     
     return pass_stats
 
 #call function to reformat data    
-pass_stats_2016 = reformat_data(2016)
-pass_stats_2017 = reformat_data(2017)
-pass_stats_2018 = reformat_data(2018)
-pass_stats_2019 = reformat_data(2019)
-pass_stats_2020 = reformat_data(2020)
+pass_stats_2016 = reformat_data(2016, False)
+pass_stats_2017 = reformat_data(2017, False)
+pass_stats_2018 = reformat_data(2018, False)
+pass_stats_2019 = reformat_data(2019, False)
+pass_stats_2020 = reformat_data(2020, False)
 
 #call function to add dataframes together into one that combines 
 # 5 years of passing data
@@ -96,29 +108,9 @@ df_passing = combine_df(df_passing, pass_stats_2020.iloc[:, 1:])
 #add back the position column
 df_passing.insert(0,'Pos', 'QB')
 
-# recalculate final summarized df to accurately represent values.
-# The combine_df function added all corresponding values, which you
-# would not want to do that for the certain columns like yards per attempt
-df_passing['Yds_Att'] = round((df_passing['Pass_Yds'] \
-                               / df_passing['Pass_Att']), 2)
-df_passing['Yds_Cmp'] = round((df_passing['Pass_Yds'] \
-                                / df_passing['Cmp']), 2 )
-df_passing['Int_Pct'] = round((df_passing['Int'] \
-                               / df_passing['Pass_Att'] * 100), 4)
-df_passing['Pass_TD_Pct'] = round((df_passing['Pass_TD'] \
-                                   / df_passing['Pass_Att'] * 100), 4)
-df_passing['Pass_Yds_Att'] = round((df_passing['Pass_Yds'] \
-                                   / df_passing['Pass_Att']), 2)
-df_passing['Pass_Yds_Cmp'] = round((df_passing['Pass_Yds'] \
-                                    / df_passing['Cmp']), 2) 
-df_passing['TD_to_TO_Ratio'] = round(((df_passing['Pass_TD'] + \
-                                        df_passing['Rush_TD']) / \
-                                       (df_passing['Int'] + \
-                                        df_passing['Fmb'])), 4)
-df_passing['Tot_Yds_Gm'] = round(((df_passing['Pass_Yds'] + \
-                                       df_passing['Rush_Yds']) / \
-                                      df_passing['G']), 4)
-    
+#reformat columns for final dataframe
+df_passing = reformat_data(-1, df_passing)
+
 #filter out Qbs that havent played a full season worth of games
 df_passing = df_passing[df_passing.GS >= 15]
 
